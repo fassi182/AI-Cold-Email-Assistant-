77d1d90dbfe0df5b5ba66898cdd08e603488a5bf
Muhammad Fassi Ur  Rehman <fassirehman07@gmail.com>
2025-08-16 13:58:41 +0500

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if not GROQ_API_KEY:
     st.stop()
 
 # ---------- UI ----------
-st.title("📧 AI Cold Email Assistant (Groq Cloud)")
+st.title("📧 AI Cold Email Assistant")
 st.caption("Provide ANY one of: Resume, Job Description, or a Portfolio/LinkedIn/GitHub link. Name/Role/Company help personalize the email.")
 
 name = st.text_input("Your Name")
@@ -166,3 +166,4 @@ if generate_button:
         st.code(email_text, language="markdown")
     except Exception as e:
         st.error(f"Error generating email: {e}")
+
