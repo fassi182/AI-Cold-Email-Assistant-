4747b3112b86923fd15e62af6bb9030189677973
fassi182 <fassirehman07@gmail.com>
2026-02-01 13:16:29 +0500

    Deploy cahnages

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import PyPDF2
 import streamlit as st
 from dotenv import load_dotenv
 
-# ✅ AI logic import (NO LangChain / Groq here)
+# ✅ AI logic import 
 from ai_logic import generate_cold_email
 
 # ---------- Setup ----------
